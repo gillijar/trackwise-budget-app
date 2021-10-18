fed6a1c35d41fbf3c2ee5335d895164c1c16e342
Jarrod Gilliland <gillijar@gmail.com>
2021-10-18 11:15:23 -0400

    styled login and create account pages

--- src/components/Auth/AuthCreateAccount.js
+++ src/components/Auth/AuthCreateAccount.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Input from "../UI/Input";
 
 const AuthCreateAccount = () => {
-  return <h1>Create Account</h1>;
+  return (
+    <div className="confirm">
+      <div className="confirm__container">
+        <p className="confirm__container--p">Create New Account</p>
+        <form className="confirm__container--form">
+          <Input type="email" placeholder="Email" />
+          <Input type="password" placeholder="Password" />
+          <Input type="password" placeholder="Confirm Password" />
+          <button type="submit">Create Account</button>
+        </form>
+        <p className="confirm__change-auth">
+          Already have an account?{" "}
+          <Link to="/auth/login">
+            <span>Sign in here</span>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AuthCreateAccount;
