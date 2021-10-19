21a1a997cb776f3b41cff9909e7beaa582014a7a
Jarrod Gilliland <gillijar@gmail.com>
2021-10-19 18:28:05 -0400

    continuing dashboard styling and added pie chart animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Switch, Route, Redirect } from "react-router-dom";
 import Auth from "./components/Pages/Auth";
 import Greet from "./components/Pages/Greet";
-import Dashboard from "./components/Pages/Dashboard";
+import Dashboard from "./components/Pages/Dashboard/Dashboard";
 
 import "./App.scss";
 
---
+++ src/components/UI/DashboardCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardCard = (props) => {
+  return <div className="dashboard-card">{props.children}</div>;
+};
+
+export default DashboardCard;
