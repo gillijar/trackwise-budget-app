98d45887884b52d22ce11dbf8dddd48608415e27
Jarrod Gilliland <gillijar@gmail.com>
2021-10-23 17:59:03 -0400

    added refs to create account page for data to be transferred to backend

--- src/components/Auth/CreateAccount.js
+++ src/components/Auth/CreateAccount.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useHistory } from "react-router";
 import Input from "../UI/Input";
 import Button from "../UI/Button";
@@ -6,8 +6,20 @@ import Button from "../UI/Button";
 const CreateAccount = () => {
   const history = useHistory();
 
+  const enteredFirstNameRef = useRef();
+  const enteredLastNameRef = useRef();
+  const enteredAgeRef = useRef();
+  const enteredGoalRef = useRef();
+
   const submitForm = (e) => {
     e.preventDefault();
+
+    const enteredFirstName = enteredFirstNameRef.current.value;
+    const enteredLastName = enteredLastNameRef.current.value;
+    const enteredAge = enteredAgeRef.current.value;
+    const enteredGoal = enteredGoalRef.current.value;
+
+    console.log(enteredFirstName, enteredLastName, enteredAge, enteredGoal);
 
     history.push("/dashboard");
   };
@@ -18,10 +30,19 @@ const CreateAccount = () => {
         <p className="confirm__container--p">Sign In to Your Account</p>
         <form className="confirm__container--form" onSubmit={submitForm}>
           <div className="confirm__container--form-input">
-            <Input type="text" placeholder="First Name" required="required" />
+            <Input
+              type="text"
+              placeholder="First Name"
+              required="required"
+              ref={enteredFirstNameRef}
+            />
           </div>
           <div className="confirm__container--form-input">
-            <Input type="text" placeholder="Last Name (optional)" />
+            <Input
+              type="text"
+              placeholder="Last Name (optional)"
+              ref={enteredLastNameRef}
+            />
           </div>
           <div className="confirm__container--form-input">
             <Input
@@ -29,12 +50,13 @@ const CreateAccount = () => {
               min="0"
               max="100"
               placeholder="Age (optional)"
+              ref={enteredAgeRef}
             />
           </div>
           <div className="confirm__container--dropdown">
             {/* <label for="goals">What is your goal with this app?</label> */}
-            <select name="goals" id="goals">
-              <option disabled selected hidden>
+            <select name="goals" id="goals" ref={enteredGoalRef}>
+              <option defaultValue hidden>
                 What are your goals with this app?
               </option>
               <option>Better budget managing</option>
